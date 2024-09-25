859fe59c4b090bbdc60d02e9b1de07e8ea8532c0
Furkan Erdoğuş <107814668+PLADSS@users.noreply.github.com>
2024-09-25 12:49:29 +0300

    03 lists
    linked lists

--- arrays.py
+++ arrays.py
@@ -1,1 +1,12 @@
 new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list:
+    print(True)
+
+for n in new_list:
+    print(n)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.extend([11, 12, 13])
